ee32fd4a1801aa4e3a918d3ca953aff92d5003bb
김다온 <kdaon07@gmail.com>
2024-01-20 04:44:45 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -13,14 +13,25 @@ function App() {
   // function 제목바꾸기() {
   //   글제목변경( ['여자코트 추천', '강남 우동 맛집', '파이썬 독학']);
   // }
-  
+
+  function 제목바꾸기() { // State는 직접 건드리지 말기 ex) 글제목[0] = 12345
+    var newArray = [...글제목]; // state를 복제하면 변수명 = state가 아닌 [...state]를 사용해야한다(deep copy)
+    newArray[0] = '여자 코트 추천';
+    글제목변경( newArray );
+  }
+
+  function 정렬() {
+    var newArray = [...글제목];
+    
+    글제목변경( newArray );
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={ 제목바꾸기 }>버튼</button>
+      <button onClick={ 정렬 }>버튼</button>
       <div className='list'>
         <h3> { 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1) }}>👍</span> { 따봉 } </h3>
         <p>2월 17일 발행</p>
